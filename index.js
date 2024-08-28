86f46f9a18b9620d76097140e282af8a933453ba
Zenha123 <fathimazenha21@gmail.com>
2024-08-28 17:49:11 +0530

    commit1

---
+++ index.js
@@ -0,0 +1,27 @@
+let randomno1=Math.floor(Math.random()*6+1);
+
+let randomimage1="images/dice"+randomno1+".png";
+
+let image1=document.querySelectorAll("img")[0];
+
+image1.setAttribute("src",randomimage1);
+
+let randomno2=Math.floor(Math.random()*6+1);
+
+let randomimage2="images/dice"+randomno2+".png";
+
+let image2=document.querySelectorAll("img")[1];
+
+image2.setAttribute("src",randomimage2);
+
+if(randomno1 > randomno2)
+{
+    document.getElementsByTagName("h2")[0].innerHTML="Player1 Win🏆";
+}
+else if(randomno1 < randomno2)
+{
+    document.getElementsByTagName("h2")[0].innerHTML="Player2 Win🏆";
+}
+else{
+    document.getElementsByTagName("h2")[0].innerHTML="try again!!!";
+}
